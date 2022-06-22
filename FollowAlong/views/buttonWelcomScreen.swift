2bece833ff4c527a89eb39200e00a2b56aa660bf
Jenny Shalai <jenny.shalai@gmail.com>
2022-06-22 08:28:15 -0400

    add custom button for welcome screen

---
+++ FollowAlong/views/buttonWelcomScreen.swift
@@ -0,0 +1,19 @@
+//
+//  buttonWelcomScreen.swift
+//  FollowAlong
+//
+//  Created by Jenny Shalai on 6/22/22.
+//
+
+import UIKit
+
+class buttonWelcomeScreen: UIButton {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.white.cgColor
+        
+    }
+    
+}
